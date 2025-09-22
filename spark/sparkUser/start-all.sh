00a8157ad46f3bebaa9c635e6f96d620bb5ba951
razvan <Entr@p1Urs#3>
2025-09-22 10:43:37 +0100

    rbi

--- spark/sparkUser/start-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$SPARK_HOME/sbin/start-master.sh
-sleep 10
-$SPARK_HOME/sbin/start-worker.sh  spark://`hostname -f`:7077
-sleep 10
-
-
-jps
--- spark/sparkUser/stop-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$SPARK_HOME/sbin/stop-worker.sh
-sleep 5
-$SPARK_HOME/sbin/stop-master.sh
-sleep 5
-
-
-jps
